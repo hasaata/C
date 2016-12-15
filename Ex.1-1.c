05f3e35450172c741a9844f903f95d83006f0a65
hasaata <dimitrisg1988@gmail.com>
2016-12-15 14:30:50 +0100

    second

--- Ex.1-1.c
+++ Ex.1-1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-	
+	int a = 0, b = 0;
+	printf("Give the first number\n");
+	scanf("%d", &a);
+	printf("Give the second number\n");
+	scanf("%d", &b);
+	int c = a + b;
+	printf("%d + %d = %d\n", a, b, c);	
 	return 0;
 }
